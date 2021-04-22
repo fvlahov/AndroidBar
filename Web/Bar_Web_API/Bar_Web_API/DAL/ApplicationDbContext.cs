0af486f718549451b049c580ac8dc63757cbc5b2
fvlahov <franko.vlahov@gmail.com>
2021-04-22 10:54:41 +0200

    Entity setup

---
+++ Web/Bar_Web_API/Bar_Web_API/DAL/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bar_Web_API.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Web/Bar_Web_API/Bar_Web_API/DAL/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bar_Web_API.DAL
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Items.Any())
+            {
+                return;   // DB has been seeded
+            }
+        }
+    }
+}
